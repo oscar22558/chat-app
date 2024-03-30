442251a8d38d8b27fd42d14936fbf3c1279baa3f
Oscar Shum <oscar22558oscar@gmail.com>
2024-03-30 01:43:51 -0400

    Updated contact page styling

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -2,13 +2,11 @@ import {Routes} from "@angular/router";
 import {SignInOrRegisterComponent} from "./sign-in-or-register/sign-in-or-register.component";
 import {ChatComponent} from "./chat/chat.component";
 import {FriendComponent} from "./friend/friend.component";
-import {ConversionComponent} from "./chat/conversion/conversion.component";
 import {ConversionDebugComponent} from "./chat/conversion-debug/conversion-debug.component";
 
 export const appRoutes: Routes = [
   {path: 'sign-in', component: SignInOrRegisterComponent},
   {path: '', component: ChatComponent},
   {path: 'friend', component: FriendComponent},
-  {path: 'chat', component: ConversionComponent},
   {path: 'chat-debug', component: ConversionDebugComponent},
 ];
